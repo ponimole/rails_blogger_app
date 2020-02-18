dab2121fa4e6c823fc42b372ff37aced6b411703
ponimole <onimolep@yahoo.com>
2020-02-18 11:02:06 -0600

    form-based workflow feature completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   Blogger::Application.routes.draw do
-      resources :articles
+    root to: 'articles#index'
+    
+    resources :articles
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
