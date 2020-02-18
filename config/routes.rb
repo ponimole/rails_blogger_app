2c6852f16b736fdd9d71b63d460fe1b94ee6004c
ponimole <onimolep@yahoo.com>
2020-02-18 13:26:03 -0600

    Finished blog comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   Blogger::Application.routes.draw do
     root to: 'articles#index'
     
-    resources :articles
+    resources :articles do
+      resources :comments
+    end
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
