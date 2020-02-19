e401ad2602f9834c8fa4541bf95da12d5ee7b4f0
ponimole <onimolep@yahoo.com>
2020-02-19 12:16:18 -0600

    Tagging feature completed

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
       resources :comments
     end
   end
-
+  resources :tags
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
